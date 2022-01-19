02c58ad867878517b043d03da29a166d42ea2f85
darkcris1 <sircnujnuj@gmail.com>
2022-01-20 02:50:06 +0800

    introduction code added

---
+++ Intro.java
@@ -0,0 +1,9 @@
+class Intro {
+    public static void main(String args[]) {
+        System.out.println("* Name: Cris T. Fandiño Jr. *");     
+        System.out.println("* Age: 19 *");     
+        System.out.println("* Address: R.castillo Agdao, Davao City *");     
+        System.out.println("* PC or Mobile User: Both, Sometimes Mobile *");     
+        System.out.println("* Internet Modem or Data: Modem *");     
+    }
+}
