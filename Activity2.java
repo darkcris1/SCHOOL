d9973d08e0ca88af5581a1920e367a1aabc29958
darkcris1 <sircnujnuj@gmail.com>
2022-01-21 00:59:37 +0800

    activity2

---
+++ Activity2.java
@@ -0,0 +1,34 @@
+public class Activity2 {
+    public static void main(String[] args) {
+        char c = 'C';
+        char r = 'R';
+        char i = 'I';
+        char s = 'S';
+
+        System.out.println(c + "" + r+ "" + i + "" + s + "");
+
+        int x = 5;
+        int xy = 3;
+        
+        int m = 7;
+        int mn = 4;
+        
+        double t = 20;
+        double tu = 7;
+        
+        float y = 11;
+        float yz = 3;
+
+        // add 
+        System.out.println(x + "+" + xy + "=" + (x+ xy));
+
+        // sub
+        System.out.println(m + "-" + mn + "=" + (m - mn));
+
+        // times
+        System.out.println(t + "*" + tu + "=" + (t * tu));
+
+        // div
+        System.out.println(y + "/" + yz + "=" + (y / yz));
+    }   
+}
