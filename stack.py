e5f20c4fe13d7ce7729d68694ba38bac2f7b806d
darkcris1 <sircnujnuj@gmail.com>
2022-09-16 09:52:42 +0800

    add stack

--- stack.py
+++ stack.py
@@ -6,7 +6,7 @@ class Stack:
     self.list.append(item)
 
   def peek(self):
-    for item in self.list[len(self.list)::-1]:
+    for item in self.list[::-1]:
       print(item)
 
   def is_empty(self):
@@ -18,10 +18,26 @@ class Stack:
   def remove(self,item):
     self.list.remove(item)
 
-stack = Stack()
+
+# Test Case
+stack = Stack() # initialize stack class
 
 stack.push(1)
 stack.push(2)
 stack.push(3)
 
+# check if the item has been pushed
 stack.peek()
+
+
+# Check if stack is empty
+print(stack.is_empty())
+
+# Check the count of stack, expected to be 3
+print(stack.count())
+
+# Check if the stack is removed on the stack
+print(stack.remove(2))
+
+# expected 2 items, because the 2 has been removed
+print(stack.list)
