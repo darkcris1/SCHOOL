b6be9e66682e8d9c8e602d170a634fd2c5cc0ace
darkcris1 <sircnujnuj@gmail.com>
2024-11-04 03:37:45 +0800

    initial commit

---
+++ my_vue_app/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="products"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Name',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Description', value: 'description' },
+          { text: 'Price', value: 'price' },
+        ],
+        products: []
+      }
+    },
+    methods: {
+      getData(){
+        this.$axios.get('http://localhost/product/index').then(response => {
+          console.log(response.data)
+          this.products = response.data
+        })
+      }
+    },
+    mounted(){
+      this.getData()
+    }
+  }
+</script>
---
+++ my_vue_app/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import './plugins/axios'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import axios from './plugins/axios'
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
